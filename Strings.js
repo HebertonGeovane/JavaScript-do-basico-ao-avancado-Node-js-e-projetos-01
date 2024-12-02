42966cad11ad89ad02789968b927a70e5aaea849
Heberton Geovane <heberton.geovane@gmail.com>
2024-12-02 18:15:45 -0300

    Operadores-logicos

---
+++ Strings.js
@@ -0,0 +1,16 @@
+// Strings 
+
+console.log("Primeira linha \n Segunda linha");
+console.log("O meu nome é 'Heberton'");
+console.log('O nome dela é "Maria"');
+console.log(`A multiplicação de 5 por 3 é ${5*3}`);
+console.log("O " + "meu" + " nome " + "é" + " Heberton.");
+
+/*
+Primeira linha
+ Segunda linha
+O meu nome é 'Heberton'
+O nome dela é "Maria"   
+A multiplicação de 5 por 3 é 15
+O meu nome é Heberton.
+*/
