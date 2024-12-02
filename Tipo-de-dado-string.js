f89e2cefe9a0b48cc2305d02ce7631b605f1fda1
Heberton Geovane <heberton.geovane@gmail.com>
2024-12-02 15:06:16 -0300

    Tipo-de-dado-string

---
+++ Tipo-de-dado-string.js
@@ -0,0 +1,29 @@
+//   Tipo-de-dado-string 
+
+console.log("Olá, meu nome é Heberton");
+console.log('Eu gostaria de comprar um carro.');
+console.log(`Teste`);
+console.log("Infinity");
+console.log(Infinity);
+
+/*
+Oi, meu nome é Heberton
+Eu gostaria comprar um carro.
+    Teste
+Infinity
+Infinity
+*/
+
+console.log(typeof "Olá, meu nome é Heberton");
+console.log(typeof 'Eu gostaria comprar um carro.');
+console.log(typeof `Teste`);
+console.log(typeof "Infinity");
+console.log(typeof Infinity);
+
+/*
+string
+string
+string
+string
+number
+*/
