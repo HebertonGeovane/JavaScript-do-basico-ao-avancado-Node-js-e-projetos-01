e5a15c4d8c7683cf21a3b9eed1e95a9d3f957d7b
Heberton Geovane <heberton.geovane@gmail.com>
2024-12-02 15:19:03 -0300

    tipo de dado string

--- Tipo-de-dado-string.js
+++ Tipo-de-dado-string.js
@@ -8,14 +8,14 @@ console.log(Infinity);
 
 /*
 Oi, meu nome é Heberton
-Eu gostaria comprar um carro.
+Eu gostaria de comprar um carro.
     Teste
 Infinity
 Infinity
 */
 
 console.log(typeof "Olá, meu nome é Heberton");
-console.log(typeof 'Eu gostaria comprar um carro.');
+console.log(typeof 'Eu gostaria de comprar um carro.');
 console.log(typeof `Teste`);
 console.log(typeof "Infinity");
 console.log(typeof Infinity);
